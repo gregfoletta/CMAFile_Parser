1c41c4405d896a47bad1ddea165f82aef19f2ef6
gregfoletta <greg@foletta.org>
2018-05-01 10:25:39 +1000

    Added dump_json(), ENABLE_DEBUG() and DISABLE_DEBUG().

--- cma_tree.h
+++ cma_tree.h
@@ -1,5 +1,4 @@
 #include "list.h"
-#include <jansson.h>
 
 struct cma_entity;
 
@@ -8,9 +7,10 @@ struct cma_entity *create_node(char *, char *, struct list_head *);
 struct list_head *add_to_entity_list(struct cma_entity *, struct list_head *);
 
 void print_entity_list(struct list_head *, int); 
-json_t *print_json(struct list_head *); 
+void dump_json(struct list_head *); 
 void free_tree(struct list_head *);
 
-void FATAL_ERROR(char *);
+void ENABLE_DEBUG(void);
+void DISABLE_DEBUG(void);
 
 	
